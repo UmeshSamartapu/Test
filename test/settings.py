fe96f89ceafb9169357e74d84c2b004fa64ac090
UmeshSamartapu <umeshsamartapu@gmail.com>
2026-05-02 10:55:26 +0530

    commit 03 - Django project

--- test/settings.py
+++ test/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import dj_database_url
 from pathlib import Path
 from decouple import config
 
@@ -79,7 +78,10 @@ WSGI_APPLICATION = 'test.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
